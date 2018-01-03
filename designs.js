2b677ff1228f3f98788d0b80b9c2ea1edca0fef4
Seamus Connolly <sghconnolly@gmail.com>
2018-01-03 11:39:12 +0000

    Add code to detect colour from the colour picker

--- designs.js
+++ designs.js
@@ -1,10 +1,32 @@
 // Select color input
-// Select size input
+// call this function when selecting pixels in the grid
 
-// When size is submitted by the user, call makeGrid()
+function getColour() {
+	let target = document.getElementById('colorPicker');
+	let colourVal = target.value;
+	return colourVal;
+}
 
-function makeGrid() {
+// Declare makeGrid() function to build the grid
+
+function makeGrid(heightVal, widthVal) {
 
 // Your code goes here!
 
 }
+
+// Select size input
+
+const gridSource = document.getElementById('sizePicker');
+
+gridSource.addEventListener('submit', function(event) {
+	// capture values of the dimensions from the form on submit
+	const gridHeight = document.getElementById('input_height');
+	let heightVal = gridHeight.value;
+	const gridWidth = document.getElementById('input_width');
+	let widthVal = gridWidth.value;
+	
+	makeGrid(heightVal, widthVal); // When size is submitted by the user, call makeGrid()
+});
+
+
