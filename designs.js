744d29b809b3437054255dec97539912999043b8
Seamus Connolly <sghconnolly@gmail.com>
2018-01-03 16:31:34 +0000

    Use of fragment to build the grid structure

--- designs.js
+++ designs.js
@@ -12,25 +12,30 @@ function getColour() {
 function makeGrid(heightVal, widthVal) {
 
 	const gridBuild = document.getElementById('pixel_canvas');
+	let fragment = document.createDocumentFragment(); // 
 	
     for (let rows = 0; rows < heightVal; rows++) {
 		let newRow = document.createElement('tr'); // Create a new <td> element
-		gridBuild.appendChild(newRow); // Add the new row to the table
+		fragment.appendChild(newRow); // Add the new row to the table
 
         for (let columns = 0; columns < widthVal; columns++) {
-            newRow.appendChild("<td class='grid'></td>");
+			let cell = document.createElement('td'); // Create a "td" element (cell) for the table row ("tr")
+			cell.className = 'grid'; // Assign the "grid" class name to each td element
+            fragment.appendChild(cell);
         };
     };
-	const grid = document.getElementsByClassName('grid');      ///////////****************************** FRAGMENT????????????*************************
-    grid.style.width = "50px";
-    grid.style.height = "50px";
+	gridBuild.appendChild(fragment); // Use fragment to construct the code for the table grid
+	
+	const grid = document.getElementsByClassName('grid') // Just to allow us to use the .style.width and .style.height to set cell sizes
+    grid.style.width = "(960/widthVal)";
+    grid.style.height = "(960/heightVal)";
 };
 
 // Select size input
 
 const gridSource = document.getElementById('sizePicker');
 
-gridSource.addEventListener('submit', function(event) {
+gridSource.addEventListener('submit', function() {
 	// capture values of the dimensions from the form on submit
 	const gridHeight = document.getElementById('input_height');
 	let heightVal = Number(gridHeight.value); // Coerce value from string to number
